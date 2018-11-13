1536b7bfce078206e1f60352f6d5641d61f3f1c1
twnelson0 <43486832+twnelson0@users.noreply.github.com>
2018-11-13 15:04:55 -0500

    Update with muon decay numbers
    I've modified the simulation to calculate and normalize the muon decay numbers. I've also added a piece of code that makes a bar graph of the muon decays associated with each number of sparks. I think there is something odd going on with the formatting though.

--- test.py
+++ test.py
@@ -6,11 +6,13 @@ Created on Thu Nov  8 13:20:53 2018
 @author: yihangao
 """
 
-#Import modules
+#Import all needed modules
 from  __future__ import division
 import random;
 import numpy;
 import math;
+import array;
+import matplotlib.pyplot as plt
 
 #Global Variables
 Pi = math.pi;
@@ -21,6 +23,7 @@ R_Det = 7.62; #Radius of detector in cm
 R_Fid = R_Det/2; #Maxium radius of electrons in cm
 X_0 = 8.9; #Radiation length in cm
 Theta_Max = Pi/6; #Maxium Polar angle
+N_Exp = 43; #Number of muon decays observed experimentally
 
 
 N_Run = 10000; #Total Number of runs per simulation
@@ -40,11 +43,16 @@ def N_Spark(L,Theta,Z_0):
 def Escape_L(r,phi,theta):
     L_escp = (-r*math.cos(phi) + math.sqrt(r**2 * (math.cos(phi))**2 + (R_Det**2 - r**2)))/(math.sin(theta))*(T_P/(T_P + T_G));
     return L_escp;
+
+#Define N_Sim function
+def NSim(rad,E_e,m_mu):
+    N_Val = rad*(m_mu*E_e)**2 *(3 - (4*E_e)/(m_mu))
+    return N_Val #Returns un normalized N_Sim Value
     
 
 #Generate array of muon masses in MeV
-LowMuonMass = 90; #Lowest Muon mass
-HighMuonMass = 105; #Highest muon mass
+LowMuonMass = 80; #Lowest Muon mass
+HighMuonMass = 110; #Highest muon mass
 MassRuns = (HighMuonMass - LowMuonMass) + 1; #Number of different masses to be tested  
 MuonMass_Array = numpy.linspace(LowMuonMass,HighMuonMass,MassRuns); #Muon mass array increments by 1 MeV (All masses are in MeV)
 
@@ -53,6 +61,7 @@ MuonMass_Array = numpy.linspace(LowMuonMass,HighMuonMass,MassRuns); #Muon mass a
 Spark_Matrix = numpy.zeros((N_Run,MassRuns)); #Initialize an empty matrix corresponding to number of sparks
 Energy_Matrix = numpy.zeros((N_Run,MassRuns)); #Initialize an empty matrix corresponding to electron energies
 Radius_Matrix = numpy.zeros((N_Run,MassRuns)); #Initialize an empty matrix corresponding to electron radius
+MuonDec_Matrix = numpy.zeros((N_Run,MassRuns)); #Initialize an empty matrix corresponding to unnormalized N_Sim
 
 #Debug_Matrix = numpy.zeros((N_Run,5)); #For debugging code
 
@@ -74,6 +83,10 @@ for i1 in range(0,MassRuns): #Loop over every muon mass
         Len_Val = StoppingLen(Electron_En);
         Escp_Len = Escape_L(r_Val,Phi_Val,Theta_Val);
         
+        #Obtain unormalized N_Sim Value
+        MuonNum = NSim(r_Val,Electron_En,MuonMass_Array[i1]);
+        MuonDec_Matrix[i2,i1] = MuonNum;
+        
         #Compare escape length and Length
         if (Len_Val > Escp_Len):
             Len_Val = Escp_Len; #Set length to escape length
@@ -82,7 +95,41 @@ for i1 in range(0,MassRuns): #Loop over every muon mass
         #Calculate the number of sparks produced and add to data matrix
         Spark_Num = N_Spark(Len_Val,Theta_Val,Z_Val);
         Spark_Matrix[i2,i1] = Spark_Num;
-        
-        
     
+    #Normalize NSim Matrix row before incrementing the mass
+    EventSum = 0; #Sum of unormalized muon detection events
+    for i3 in range(0,N_Run):
+        EventSum = EventSum + MuonDec_Matrix[i3,i1];
+    #Get the normalization constant
+    D = N_Exp/(EventSum);
+    #Scale the Muon Decay column by the calculated normalization factor
+    MuonDec_Matrix[:,i1] = D*MuonDec_Matrix[:,i1];                    
+        
 
+##Data Analysis Section##
+
+#For each number of sparks determine how many muon decays occured
+#for j0 in range(0,MassRuns)
+j0 = 1; #Which mass run is being considered
+SparkArray = [0,1,3,4,5,6,7,8];
+AssociatedDecay = array.array('d');
+for j1 in range(0,8): #Loop over all possible numbers of sparks
+    IndxArray = array.array('d'); #Location of all j1 sparks in the spark matrix
+    #Find the indicies of all n_Spark = j1
+    for j2 in range(0,N_Run):
+        if (j1 == Spark_Matrix[j2,j0]):
+            IndxArray.append(j2);
+    
+    #Sum over all the indices in the muon decay array to determine the number of decays with n_spark
+    Muon_Sum = 0;
+    for j3 in range(0,len(IndxArray)):
+        IndVal = int(IndxArray[j3])
+        Muon_Sum = Muon_Sum + MuonDec_Matrix[IndVal,j0];
+    AssociatedDecay.append(Muon_Sum); #Append the number of muon decays
+    print(Muon_Sum)
+    
+#Plot the number of muon decays vs number of sparks
+plt.bar(SparkArray,AssociatedDecay);
+plt.title("")
+plt.xlabel("Number of Sparks");
+plt.ylabel("Number of Muon Decays");
